f3107c8d0e3cd59abd251e3d6fa541b3b6893140
David <davidporter33@icloud.com>
2017-03-28 13:05:44 -0700

    Fixed yesterdays tech requirements resubmitting.

--- DPPicFeed/DPPicFeed/HomeViewController.swift
+++ DPPicFeed/DPPicFeed/HomeViewController.swift
@@ -14,6 +14,7 @@ class HomeViewController: UIViewController, UIImagePickerControllerDelegate, UIN
     @IBOutlet weak var imageView: UIImageView!
     
     override func viewDidLoad() {
+        //This is override because we are overriding the superclass
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
@@ -32,14 +33,28 @@ class HomeViewController: UIViewController, UIImagePickerControllerDelegate, UIN
     }
     func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
         
-        self.imageView.image = info["UIImagePickerControllerOriginalImage"] as? UIImage
+        self.imageView.image = info[UIImagePickerControllerOriginalImage] as? UIImage
         self.dismiss(animated: true, completion: nil)
     }
     
     @IBAction func imageTapped(_ sender: Any) {
-        print("User Tappeed Image")
+        print("User Tapped Image")
         self.presentActionSheet()
         //this is basically an event listener.
+    }
+   
+    @IBAction func postButtonPressed(_ sender: Any) {
+        if let image = self.imageView.image {
+            let newPost = Post(image: image)
+            CloudKit.shared.save(post: newPost, completion: { (success) in
+                if success {
+                    print("Saved Post successfully to CLoudit")
+                }
+                else {
+                    print("We did NOT Successfully save to CLoutKit...")
+                }
+            })
+        }
     }
     
     func presentActionSheet() {
@@ -51,6 +66,10 @@ class HomeViewController: UIViewController, UIImagePickerControllerDelegate, UIN
             self.presentImagePickerWith(sourceType: .photoLibrary)
         }
         let cancelAction = UIAlertAction(title: "Cancel", style: .destructive, handler: nil)
+        
+        if !UIImagePickerController.isSourceTypeAvailable(.camera) {
+            cameraAction.isEnabled = false
+        }
         
         actionSheetController.addAction(cameraAction)
         actionSheetController.addAction(photoAction)
